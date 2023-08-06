f96fc02ffae2acbc16f212315e6a8455ffd42667
Andrew Marabante <andrewmarabante@gmail.com>
2023-08-05 20:10:33 -0700

    Created a function that displays all books

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let book2 = new book('Big Boy Ben', 'Tiwon Michaels', '1934', 'Funny');
 let book3 = new book('Crystal Cove', 'John Cry', '1934', 'Adventure');
 let book4 = new book('Diaries of Manhattan', 'Mike Hawk', '1934', 'Drama');
 let book5 = new book('Guns', 'Jack Johnson', '1934', 'Action');
-
+document.getElementById('form').addEventListener('submit',addBook)
 
 function book(title,author,year,genre)
 {
@@ -13,11 +13,34 @@ function book(title,author,year,genre)
     this.year = year;
     this.genre = genre;
     myLibrary.push(this);
+    displayBook(this);
 }
 
-function checkBook(e)
+function addBook(e)
 {
-    console.log(myLibrary[2]);
+    e.preventDefault();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const year = document.getElementById('year').value;
+    const genre = document.getElementById('genre').value;
+    let newBook = new book(title,author,year,genre);
+    console.log(myLibrary[5]);
 }
-document.getElementById('start').addEventListener('click', checkBook);
 
+function displayBook(book)
+{
+    const title = document.createElement("h4");
+    const author = document.createElement("p");
+    const year = document.createElement("p");
+    const genre = document.createElement("p");
+    
+    title.innerHTML = book.title;
+    author.innerHTML = book.author;
+    year.innerHTML = book.year;
+    genre.innerHTML= book.genre;
+
+    document.body.append(title);
+    document.body.append(author);
+    document.body.append(year);
+    document.body.append(genre);
+}
